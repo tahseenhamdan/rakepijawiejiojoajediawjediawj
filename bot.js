36c89577a049516fd3ea5347b8b8a46aa39d2c86
tahseenhamdan <41524266+tahseenhamdan@users.noreply.github.com>
2018-07-22 10:47:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,6 +50,19 @@ message.author.send(`  **
 **`);
     }
 });
+
+bot.on("message", message => {
+    var prefix = "+"
+    if (!message.content.startsWith(prefix)) return;
+      let command = message.content.split(" ")[0];
+      command = command.slice(prefix.length);
+        if(command === "mcskin") {
+                const args = message.content.split(" ").slice(1).join(" ")
+        if (!args) return message.channel.send("** Type your skin name **");
+        const image = new Discord.Attachment(`https://minotar.net/armor/body/${args}`, "skin.png");
+    message.channel.send(image)
+        }
+    });
  
 
 
