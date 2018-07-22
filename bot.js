af4fabc4a0591ee9e9a8ad11c3e990291bd8c1fe
tahseenhamdan <41524266+tahseenhamdan@users.noreply.github.com>
2018-07-22 11:37:41 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -51,6 +51,40 @@ message.author.send(`  **
     }
 });
 
+const db = require('quick.db');
+client.on('message' , async (message) => {
+       if(message.content.startsWith(prefix + "pre")) {
+       let args = message.content.split(" ").slice(1);
+let fetched = await db.fetch(`prefix_${message.guild.id}`);
+if (fetched === null) prefix = '+';
+else prefix = fetched;
+ 
+
+
+if (!message.member.hasPermission('ADMINISTRATOR') && message.author.id !== '402866255838576652') return message.channel.send('Sorry, you don\'t have permission to change server prefix')
+    .then(msg => msg.delete({
+        timeout: 10000
+    }));
+
+if (!args.join(' ')) return message.channel.send('Please provide a prefix to change server prefix')
+    .then(msg => msg.delete({
+        timeout: 10000
+    }));
+    
+
+db.set(`prefix_${message.guild.id}`, args.join(' '))
+    .then(i => {
+    
+    message.channel.sendMessage("", {embed: {
+      title: "Prefix Set!",
+      color: 0x06DF00,
+      description: `Set to ${i}`,
+     
+    }})
+    })
+       }
+    
+       });
 
 
 
