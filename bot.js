bd6cbc79056acd5f5e020131ef3cec153c757dda
tahseenhamdan <41524266+tahseenhamdan@users.noreply.github.com>
2018-07-23 10:41:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -52,23 +52,10 @@ client.on('message', message => {
      
 
                 
+ 
+     
+  
     
-});
-
-
-client.on('message', message => {
-   if (message.content === "+id") {
-   let embed = new Discord.RichEmbed()
-  .setColor("RANDOM")
-  .setThumbnail(message.author.avatarURL)
-  .addField("Name:",`${message.author.username}`, true)
-  .addField('Discrim:',"#" +  message.author.discriminator, true)
-  .addField("ID:", message.author.id, true)
-  .addField("Create At:", message.author.createdAt, true)
-     
-     
-  message.channel.sendEmbed(embed);
-    }
 });
 
 var prefix = '+';
