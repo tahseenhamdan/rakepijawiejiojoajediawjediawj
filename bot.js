a0a1ef328c583c27ebfbea3d17d46895600bd15d
tahseenhamdan <41524266+tahseenhamdan@users.noreply.github.com>
2018-07-23 13:32:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -121,19 +121,18 @@ developer by : ταнsєєи#4825
 
  
 
-bot.on("message", message => {
+client.on("message", message => {
     var prefix = "+"
     if (!message.content.startsWith(prefix)) return;
       let command = message.content.split(" ")[0];
       command = command.slice(prefix.length);
         if(command === "mcskin") {
                 const args = message.content.split(" ").slice(1).join(" ")
-        if (!args) return message.channel.send("** Type your skin name **");
+        if (!args) return message.channel.send(" Type your skin name ");
         const image = new Discord.Attachment(`https://minotar.net/armor/body/${args}`, "skin.png");
     message.channel.send(image)
         }
     });
-
 
 
 
