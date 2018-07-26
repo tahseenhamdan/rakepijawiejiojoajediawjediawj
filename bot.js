24d4c064f20e30fcdb468d116cd7ece7d3683dc7
tahseenhamdan <41524266+tahseenhamdan@users.noreply.github.com>
2018-07-26 23:01:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -197,7 +197,43 @@ channel.send({embed : embed});
 
 
 
-
+let rebel;
+client.on("ready", async  => {
+    let guild = client.guilds.get("ID SERVER");
+  let users = guild.members.map(member => member.user.id);
+  let i;
+  rebel=0;
+for (i=0 ; i < users.length ; i++) {
+ let   check = guild.members.get(users[i]);
+if(!check.voiceChannelID){
+        continue;
+}else{
+  rebel++;
+}
+}
+guild.channels.find('id', 'ID ROOM').setName(" Voice「"+rebel+"」");
+  client.setInterval(() =>{
+    let d = Date.now()
+  }, 5000);
+});
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+    let guild = client.guilds.get("ID SERVER");
+let newUserChannel = newMember.voiceChannel
+let oldUserChannel = oldMember.voiceChannel
+ if(oldUserChannel === undefined && newUserChannel !== undefined) {
+   rebel++;
+guild.channels.find('id', 'ID ROOM').setName(" Voice「"+rebel+"」");
+} else if(newUserChannel === undefined){
+  rebel--;
+guild.channels.find('id', 'ID ROOM').setName(" Voice「"+rebel+"」");
+}
+});
+client.on('message', Codes => {
+  
+  if(Codes.content === "+sound") {
+      Codes.channel.send(" Voice「"+rebel+"」");
+}
+});
 
 
 
