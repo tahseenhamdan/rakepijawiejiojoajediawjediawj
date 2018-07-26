561dded3c55c480dd2fd695565bc57e245429a24
tahseenhamdan <41524266+tahseenhamdan@users.noreply.github.com>
2018-07-26 22:42:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -190,24 +190,47 @@ channel.send({embed : embed});
 
 
 
+
+
 client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
+    if (message.content.startsWith(prefix + "id")) {
+var args = message.content.split(" ").slice(1);
+let user = message.mentions.users.first();
+var men = message.mentions.users.first();
+ var heg;
+ if(men) {
+     heg = men
+ } else {
+     heg = message.author
+ }
+var mentionned = message.mentions.members.first();
+  var h;
+ if(mentionned) {
+     h = mentionned
+ } else {
+     h = message.member
+ }
+        moment.locale('ar-TN');
+var id = new  Discord.RichEmbed()
+.setColor("RANDOM")
+.addField(': انضمامك لسيرفر قبل', `${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n \`${moment(h.joinedAt).fromNow()}\``, true)
+.addField(': دخولك لديسكورد قبل', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true)
+.addField(": النك نيم",`${h.nickname}`, true) .addField(": #",heg.discriminator, true)
+.addField(`: البلينق`,`${h.presence.game && h.presence.game.name || '-'}`,true) .addField(': الحالة',`${h.presence.status}`,true)
+.addField(`: الرتب`, `${message.guild.members.get(h.id).roles.map(r => `\`${r.name}\``).slice(1).join('\n') || 'لايوجد رتب'}`,true)                                                    
+.setThumbnail(heg.avatarURL);
+message.channel.send(id)
+}       });
+  
 
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
 
 
 
-if (command == "say") {
-        message.delete();
-  message.channel.send(args.join("  "))
-  }
 
 
-});@
+
+
+
 
 
 
