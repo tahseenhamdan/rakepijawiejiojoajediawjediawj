23d02af707c85f8e310ae530499a0b1d46cb9a86
tahseenhamdan <41524266+tahseenhamdan@users.noreply.github.com>
2018-07-27 17:07:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -206,9 +206,20 @@ channel.send({embed : embed});
 
 
 
-client.on('message', message => { if (message.author.bot) return; if (!message.content.startsWith(prefix)) return; let command = message.content.split(" ")[0]; command = command.slice(prefix.length); let args = message.content.split(" ").slice(1); if (command == "say") { message.channel.sendMessage(args.join(" ")) message.delete() } })
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
 
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
 
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
 
 
 
