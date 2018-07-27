a71143b98482a51b470d7f3b0e674af6b58f5546
tahseenhamdan <41524266+tahseenhamdan@users.noreply.github.com>
2018-07-27 17:30:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,18 +3,7 @@ const client = new Discord.Client();
 var prefix = "+";
 client.on("message", message => {
 
-            if (message.content.startsWith(prefix + "bc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
-};     
-});
-
+ 
 
 client.on('ready', () => {
    console.log(`----------------`);
