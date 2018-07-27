2d44aca91b91868a85998d9dc4274c64457a299c
tahseenhamdan <41524266+tahseenhamdan@users.noreply.github.com>
2018-07-27 17:27:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -124,6 +124,50 @@ developer by : ταнsєєи#4825
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.on('message', message => {
+    if (message.author.id === client.user.id) return;
+    if (message.guild) {
+   let embed = new Discord.RichEmbed()
+    let args = message.content.split(' ').slice(1).join(' ');
+if(message.content.split(' ')[0] == prefix + 'bc1') {
+    if (!args[1]) {
+return;
+}
+        message.guild.members.forEach(m => {
+   if(!message.member.hasPermission('ADMINISTRATOR')) return;
+            var bc = new Discord.RichEmbed()
+            .addField(' » الرسالة : ', args)
+            .setColor('#ff0000')
+            // m.send(`[${m}]`);
+            m.send(`${m}`,{embed: bc});
+        });
+    }
+    } else {
+        return;
+    }
+});
+
+
+
+
+
+
+
+
+
  
 
 client.on("message", message => {
