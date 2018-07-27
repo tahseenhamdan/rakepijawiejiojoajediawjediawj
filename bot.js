ff5d90107071b7ec708728402af0ae4457871e53
tahseenhamdan <41524266+tahseenhamdan@users.noreply.github.com>
2018-07-27 17:33:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,16 +5,6 @@ client.on("message", message => {
 
  
 
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : TAHSEEN`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : TAHSEEN ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`+help`,"http://twitch.tv/S-F")
-client.user.setStatus("dnd")
-});
 
 client.on('message', message => {
   if(message.content.startsWith ('+help')) {
